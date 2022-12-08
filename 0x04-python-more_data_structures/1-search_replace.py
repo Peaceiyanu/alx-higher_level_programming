e006fe00925411f89b66d81312add535f24b192a
Peace Iyanu Joseph <110996772+Peaceiyanu@users.noreply.github.com>
2022-12-08 12:49:42 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,5 @@
-jhfndsfhnd
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
