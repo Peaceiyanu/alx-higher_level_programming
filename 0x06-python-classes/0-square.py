272a1bcd2da6104975dc8a54ae1b130771c7eb04
Peaceiyanu <peaceiyanu3010@gmail.com>
2022-12-21 15:40:24 -0800

    All python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
-"""Defines a square """
+Square = __import__('0-square').Square
 
-
-class Square:
-    """Does nothing"""
-    pass
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+        @property
+        def size(self):
+            """Get/set the current size of the square."""
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+
+            def area(self):
+                """Return the current area of the square."""
+                return (self.__size * self.__size)
