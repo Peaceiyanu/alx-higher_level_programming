d83aba844c1263c79667ec9b172826a2e5c18262
Peace Iyanu Joseph <110996772+Peaceiyanu@users.noreply.github.com>
2022-12-22 00:49:33 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+"""Define a class Square."""
+class Square:
+    """Represent a square."""
