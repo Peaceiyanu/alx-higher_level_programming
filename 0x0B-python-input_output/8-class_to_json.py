5cce2869aea16943e5b958ca252562bec0f5ba04
Peace Iyanu Joseph <110996772+Peaceiyanu@users.noreply.github.com>
2023-01-11 11:39:39 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
-yhhyujn
+#!/usr/bin/python3
+"""This module defines a Python class-to-JSON function"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.__dict__
